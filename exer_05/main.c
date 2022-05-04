2892136be99dd98f3bc4aa9db98483421e6f2e69
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-05-04 10:54:14 -0300

    atualização dos exercícios

--- exer_05/main.c
+++ exer_05/main.c
@@ -1,24 +1,17 @@
-/*
-
-Faça um programa que leia um número inteiro positivo e imprima o seu fatorial. Exemplo: 5!=5x4x3x2x1=120.
-
-*/
 #include <stdio.h>
+
 int main()
-{
-    int num=0,
-        fat=1;
- 
-    printf("Digite um numero\n");
-    scanf("%i",&num);
-    for (int i = 1; i <= num; i++)
+{ 
+    for (int i = 0; i <= 50; i+=2)
     {
-     printf(" %i",i);
-     fat = fat * i;      
+        printf(" %i ", i);
+        
     }
-    printf(" %i",fat);
- 
+    printf("\n");
+    for (int j = 100; j >=0; j-=4)
+    {
+        printf("%i  ", j);
+    }
+    
     return 0;
 }
-
-
